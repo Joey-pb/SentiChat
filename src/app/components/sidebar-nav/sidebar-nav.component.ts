49422f507b0faf22f6baa34f98554789fab5d767
Joseph <joeypbassett@gmail.com>
2024-12-08 12:37:07 -0500

    Add error handling/side bar app description

--- src/app/components/sidebar-nav/sidebar-nav.component.ts
+++ src/app/components/sidebar-nav/sidebar-nav.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AuthFireService } from '../../services/auth-fire.service';
 import { Router } from '@angular/router';
+import { NotificationService } from '../../services/notification.service';
+import { FirebaseError } from 'firebase/app';
+import { getFirebaseErrors } from '../../utilities/fire-errors';
+import { StoreFireService } from '../../services/store-fire.service';
 
 @Component({
   selector: 'app-sidebar-nav',
@@ -10,11 +14,25 @@ import { Router } from '@angular/router';
 })
 export class SidebarNavComponent {
   authFireService = inject(AuthFireService);
+  storeFireService = inject(StoreFireService);
+  notificationService = inject(NotificationService);
   router = inject(Router);
   currentUser = this.authFireService.currentUser;
 
   async onLogout() {
-    await this.authFireService.logout();
-    this.router.navigate(['/home']);
+    try {
+      this.notificationService.showLoading();
+      this.storeFireService.unsubscribeChat();
+      await this.authFireService.logout();
+      this.router.navigate(['/home']);
+    } catch (error: any) {
+      this.firebaseError(error);
+    } finally {
+      this.notificationService.hideLoading();
+    }
+  }
+
+  firebaseError(error: FirebaseError) {
+    this.notificationService.showModal(getFirebaseErrors(error));
   }
 }
