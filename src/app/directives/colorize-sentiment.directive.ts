a53e7af1393d1d711ef917d4d22cffb0ca400287
Joseph <joeypbassett@gmail.com>
2024-12-08 12:33:34 -0500

    Restructure

--- src/app/directives/colorize-sentiment.directive.ts
+++ src/app/directives/colorize-sentiment.directive.ts
@@ -4,57 +4,46 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[colorizeSentiment]',
 })
 export class ColorizeSentimentDirective {
-  currentStyle = {
-    borderType: 'border-',
-    borderClass: 'border-',
-  };
-  @Input() borderType: 'full' | 'left' = 'full';
+  borderClass = '';
+  currentClass = '';
+
+  @Input() borderType: 'border-full' | 'border-left' = 'border-full';
   @Input() set colorizeSentiment(classification: string | undefined) {
-    // if (!classification) {
-    //   return;
-    // }
-
-    if (this.borderType === 'left') {
-      this.currentStyle.borderType = 'border-l-';
-      this.el.nativeElement.classList.add(`${this.currentStyle.borderType}2`);
-    } else {
-      this.el.nativeElement.classList.add(`${this.currentStyle.borderType}2`);
-    }
-
-    if (!classification) {
-      this.setBorderClass('neutral-700');
-      this.addBorderClass();
-    }
-    if (classification === 'POSITIVE') {
-      this.removeBorderClass();
-      this.setBorderClass('green-500');
-      this.addBorderClass();
-    }
-    if (classification === 'NEGATIVE') {
-      this.removeBorderClass();
-      this.setBorderClass('red-500');
-      this.addBorderClass();
-    }
-    if (classification === 'NEUTRAL') {
-      this.removeBorderClass();
-      this.setBorderClass('orange-500');
-      this.addBorderClass();
+    switch (classification) {
+      case 'POSITIVE':
+        this.changeBorder('green');
+        break;
+      case 'NEGATIVE':
+        this.changeBorder('red');
+        break;
+      case 'NEUTRAL':
+        this.changeBorder('amber');
+        break;
+      default:
+        this.changeBorder('neutral');
     }
   }
 
   constructor(private el: ElementRef) {}
 
-  setBorderClass(color: string) {
-    this.currentStyle.borderClass = `${this.currentStyle.borderType}${color}`;
+  changeBorder(color: string) {
+    this.removeCurrentClass();
+    this.setCurrentClass(`${color}-`);
+    this.addBorderClass();
   }
 
-  removeBorderClass() {
-    return this.el.nativeElement.classList.remove(
-      this.currentStyle.borderClass
-    );
+  setCurrentClass(color: string) {
+    this.currentClass = `${color}${this.borderType}`;
+  }
+
+  removeCurrentClass() {
+    if (!this.currentClass) {
+      return;
+    }
+    this.el.nativeElement.classList.remove(this.currentClass);
   }
 
   addBorderClass() {
-    return this.el.nativeElement.classList.add(this.currentStyle.borderClass);
+    return this.el.nativeElement.classList.add(this.currentClass);
   }
 }
