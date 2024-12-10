8103b6b279ba491f307fba1e914803acfb36ef7c
Joseph <joeypbassett@gmail.com>
2024-12-09 20:17:48 -0500

    Netlify configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 const redirectLoggedInToChat = () => redirectLoggedInTo(['chat']);
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'home',
     component: LandingComponent,
