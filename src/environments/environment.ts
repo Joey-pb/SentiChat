25532fe9520118e904ccf18aa8e3844c8dd16508
Joseph <jbas127@wgu.edu>
2024-12-10 08:13:16 -0500

    Netlify config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  geminiApiKey: process.env['NG_APP_GEMINI_API_KEY'],
+  geminiApiKey: 'NG_APP_GEMINI_API_KEY',
   firebase: {
-    apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-    authDomain: process.env['NG_APP_AUTH_DOMAIN'],
-    databaseURL: process.env['NG_APP_DATABASE_URL'],
-    projectId: process.env['NG_APP_PROJECT_ID'],
-    storageBucket: process.env['NG_APP_STORAGE_BUCKET'],
-    messagingSenderId: process.env['NG_APP_MESSAGING_SENDER_ID'],
-    appId: process.env['NG_APP_APP_ID'],
+    apiKey: 'NG_APP_FIREBASE_API_KEY',
+    authDomain: 'NG_APP_AUTH_DOMAIN',
+    databaseURL: 'NG_APP_DATABASE_URL',
+    projectId: 'NG_APP_PROJECT_ID',
+    storageBucket: 'NG_APP_STORAGE_BUCKET',
+    messagingSenderId: 'NG_APP_MESSAGING_SENDER_ID',
+    appId: 'NG_APP_APP_ID',
   },
 };
